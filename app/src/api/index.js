e706e9cb8c26434a26b58106c78d8bcd61887693
WH1BA095 <nikitaked57@gmail.com>
2026-06-03 12:48:11 +0300

    bug fixes

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -4,7 +4,8 @@ import Constants from 'expo-constants';
 
 // ── конфиг ────────────────────────────────────────────────────────────────
 function getServerBase() {
-  if (!__DEV__) return 'http://176.98.176.149:8899';
+  //if (!__DEV__) return 'http://176.98.176.149:8899';
+  if (true) return 'http://176.98.176.149:8899';
   // На реальном устройстве берём IP Metro-сервера автоматически
   const metroHost = Constants.expoConfig?.hostUri?.split(':')[0];
   if (metroHost) return `http://${metroHost}:8899`;
